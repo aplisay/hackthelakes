bccb90970b8fd94ab7fd304a41633dc776ea8d49
Rob Pickering <rob@pickering.org>
2022-05-02 23:55:02 +0100

    Remove manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,18 +44,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'hackthelakes',
-        short_name: 'hackthelakes',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
-      },
-    },
+
 
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
